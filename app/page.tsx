22b5e9d0ec4e3b1dbeaf0dffd31ec79eb0264554
uniquetheo <55855459+uniquetheo@users.noreply.github.com>
2025-02-19 00:23:11 +0000

    add up

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroGrid from "../components/heroGrid";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full min-h-screen items-center justify-center border ">
+    <main className="flex flex-col w-full min-h-screen items-center justify-center ">
       <div className="container mx-auto">
         <HeroGrid />
       </div>
