e93a8926bf963085cacd11bef78dc17d0e36b60a
uniquetheo <55855459+uniquetheo@users.noreply.github.com>
2025-03-25 18:57:25 +0000

    further optimization of tour page

--- app/tour/page.jsx
+++ app/tour/page.jsx
@@ -5,7 +5,7 @@ import BandsintownWidget from "../../components/BandsintownWidget";
 const TourPage = () => {
   return (
     <main className="mt-16 ">
-      <div className="container min-h-[calc(100vh-65px)] mx-auto text-center flex flex-col items-center justify-center">
+      <div className="container min-h-[calc(100vh-65px)] my-6 mx-auto text-center flex flex-col items-center justify-center">
         {/* <Tour /> */}
         {/* <h1>Tour Dates</h1> */}
         {/* <p>Tours will be added shortly.. 👌👍</p> */}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,15 @@
 // "use client";
 
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="h-16 bg-secondary flex justify-between items-center">
-      <footer className="container mx-auto px-4 ">
+      <footer className="container mx-auto px-4 flex justify-between items-center">
         <span>All rights reserved. &copy; {new Date().getFullYear()}</span>
+        <Link href="https://uniiktheo.tech" target="_blank">
+          Powered by UniikTheo
+        </Link>
       </footer>
     </div>
   );
