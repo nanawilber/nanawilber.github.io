ec547051cb251fba6ec777eefcbfab1fb5d388e3
uniquetheo <unique.theo98@gmail.com>
2026-02-10 09:25:45 +0000

    feat: update uniiktheo url

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="h-16 bg-secondary flex justify-between items-center">
       <footer className="container mx-auto px-4 flex justify-between items-center">
         <span>All rights reserved. &copy; {new Date().getFullYear()}</span>
-        <Link href="https://uniiktheo.tech" target="_blank">
+        <Link href="https://uniiktheo.vercel.app" target="_blank">
           Powered by UniikTheo
         </Link>
       </footer>
